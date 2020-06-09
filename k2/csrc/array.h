314b3dc6ad8fab3df83082de3e480ecc65351f20
Haowen Qiu <qindazhu@gmail.com>
2020-06-09 13:08:37 +0800

    support StridedPtr in Array2 and add some tests

--- k2/csrc/array.h
+++ k2/csrc/array.h
@@ -1,45 +1,49 @@
 // k2/csrc/array.h
 
-// Copyright (c)  2020  Xiaomi Corporation (author: Daniel Povey)
+// Copyright (c)  2020  Xiaomi Corporation (authors: Daniel Povey
+//                                                   Haowen Qiu)
 
 // See ../../LICENSE for clarification regarding multiple authors
 
 #ifndef K2_CSRC_ARRAY_H_
 #define K2_CSRC_ARRAY_H_
 
+#include <algorithm>
 #include <functional>
+#include <iterator>
 #include <limits>
 #include <memory>
+#include <type_traits>
+#include <utility>
 #include <vector>
+
+#include "glog/logging.h"
 
 namespace k2 {
 
 /*
-   We will use e.g. StridedPtr<int32_t, T> when the stride is not 1, and
+   We will use e.g. StridedPtr<T, I> when the stride is not 1, and
    otherwise just T* (which presumably be faster).
 */
-template <typename I, typename T>
+template <typename T, typename I = int32_t>
 struct StridedPtr {
-  T *data;
-  I stride;
-  T &operator[](I i) { return data[i]; }
-  StridedPtr(T *data, I stride) : data(data), stride(stride) {}
-};
-
-/* MIGHT NOT NEED THIS */
-template <typename I, typename Ptr>
-struct Array1 {
-  // Irregular two dimensional array of something, like vector<vector<X> >
-  // where Ptr is, or behaves like, X*.
-  using IndexT = I;
-  using PtrT = Ptr;
-
-  // 'begin' and 'end' are the first and one-past-the-last indexes into `data`
-  // that we are allowed to use.
-  IndexT begin;
-  IndexT end;
-
-  PtrT data;
+  T *data;   // it is NOT owned here
+  I stride;  // in number of elements, NOT number of bytes
+  T &operator[](I i) { return data[i * stride]; }
+  const T &operator[](I i) const { return data[i * stride]; }
+  StridedPtr(T *data = nullptr, I stride = 0)  // NOLINT
+      : data(data), stride(stride) {}
+  StridedPtr(const StridedPtr &other)
+      : data(other.data), stride(other.stride) {}
+  StridedPtr &operator=(const StridedPtr &other) {
+    StridedPtr tmp(other);
+    std::swap(data, tmp.data);
+    std::swap(stride, tmp.stride);
+    return *this;
+  }
+  bool operator==(const StridedPtr &other) const {
+    return data == other.data && stride == other.stride;
+  }
 };
 
 /*
@@ -52,70 +56,150 @@ struct Array2Size {
   // `size1` is the top-level size of the array, equal to the object's .size
   // element
   I size1;
-  // `size2` is the nunber of elements in the array, equal to
+  // `size2` is the number of elements in the array, equal to
   // o->indexes[o->size] - o->indexes[0] (if the Array2 object o is
   // initialized).
   I size2;
 };
 
-template <typename I, typename Ptr>
+template <typename Ptr, typename I = int32_t>
 struct Array2 {
   // Irregular two dimensional array of something, like vector<vector<X> >
   // where Ptr is, or behaves like, X*.
   using IndexT = I;
   using PtrT = Ptr;
+  using ValueType = typename std::iterator_traits<Ptr>::value_type;
 
   IndexT size;
-  const IndexT *indexes;  // indexes[0,1,...size] should be defined; note, this
-                          // means the array must be of at least size+1.  We
-                          // require that indexes[i] <= indexes[i+1], but it is
-                          // not required that indexes[0] == 0, it may be
-                          // greater than 0.
+  IndexT *indexes;  // indexes[0,1,...size] should be defined; note, this
+                    // means the array must be of at least size+1.  We
+                    // require that indexes[i] <= indexes[i+1], but it is
+                    // not required that indexes[0] == 0, it may be
+                    // greater than 0.
 
   PtrT data;  // `data` might be an actual pointer, or might be some object
               // supporting operator [].  data[indexes[0]] through
               // data[indexes[size] - 1] must be accessible through this
               // object.
 
-  /* initialized definition:
-
-        An Array2 object is initialized if its `size` member is set and its
-        `indexes` and `data` pointer allocated, and the values of its `indexes`
-        array are set for indexes[0] and indexes[size].
+  /*
+        An Array2 object is non-empty if its `size` member is set and its
+        `indexes` and `data` pointer allocated,
   */
+  bool Empty() const {
+    return size == 0 || indexes == nullptr || data == nullptr;
+  }
 };
 
-template <typename I, typename Ptr>
+template <typename Ptr, typename I>
 struct Array3 {
-  // Irregular three dimensional array of something, like vector<vector<vetor<X>
-  // > > where Ptr is or behaves like X*.
+  // Irregular three dimensional array of something, like
+  // vector<vector<vector<X> > > where Ptr is or behaves like X*.
   using IndexT = I;
   using PtrT = Ptr;
 
   IndexT size;
-  const IndexT *indexes1;  // indexes1[0,1,...size] should be defined; note,
-                           // this means the array must be of at least size+1.
-                           // We require that indexes[i] <= indexes[i+1], but it
-                           // is not required that indexes[0] == 0, it may be
-                           // greater than 0.
+  IndexT *indexes1;  // indexes1[0,1,...size] should be defined; note,
+                     // this means the array must be of at least size+1.
+                     // We require that indexes[i] <= indexes[i+1], but it
+                     // is not required that indexes[0] == 0, it may be
+                     // greater than 0.
 
-  const IndexT *indexes2;  // indexes2[indexes1[0]]
-                           // .. indexes2[indexes1[size]-1] should be defined.
+  IndexT *indexes2;  // indexes2[indexes1[0]]
+                     // .. indexes2[indexes1[size]-1] should be defined.
 
   Ptr data;  // `data` might be an actual pointer, or might be some object
-             // supporting operator [].  data[indexes[0]] through
-             // data[indexes[size] - 1] must be accessible through this
-             // object.
+             // supporting operator [].  data[indexes2[indexes1[0]]] through
+             // data[indexes2[indexes1[size] - 1]] must be accessible through
+             // this object.
 
-  Array2<I, Ptr> operator[](I i) {
-    // TODO(haowen): fill real data here
-    Array2<I, Ptr> array;
+  Array2<Ptr, I> operator[](I i) const {
+    DCHECK_GE(i, 0);
+    DCHECK_LT(i, size);
+
+    Array2<Ptr, I> array;
+    array.size = indexes1[i + 1] - indexes1[i];
+    array.indexes = indexes2 + indexes1[i];
+    array.data = data;
     return array;
   }
 };
 
 // Note: we can create Array4 later if we need it.
 
+/*
+  `DataPtrCreator::Create` wraps data storage std::unique_ptr<value_type(Ptr)>
+  as a `Ptr` and returns it, this class is just for test purpose for now.
+*/
+template <typename ValueType, typename I>
+struct DataPtrCreator;
+
+// General case where stride is 1 and the underlying data manager is a raw
+// pointer, we will just return data_storage.get()
+template <typename ValueType, typename I>
+struct DataPtrCreator<ValueType *, I> {
+  static ValueType *Create(const std::unique_ptr<ValueType> &data_storage,
+                           int32_t stride) {
+    CHECK_EQ(stride, 1);
+    return data_storage.get();
+  }
+};
+
+// Specialized case where stride is bigger than 1 and the underlying data
+// manager is StridedPtr, we will create a StridedPtr with data_storage and
+// return it.
+template <typename ValueType, typename I>
+struct DataPtrCreator<StridedPtr<ValueType, I>, I> {
+  static StridedPtr<ValueType, I> Create(
+      const std::unique_ptr<ValueType> &data_storage, int32_t stride) {
+    CHECK_GT(stride, 1);
+    StridedPtr<ValueType, I> strided_ptr(data_storage.get(), stride);
+    return strided_ptr;
+  }
+};
+
+// Allocate memory for Array2, this class is just for test purpose.
+// It will be usually called after those `GetSizes` functions (e.g.
+// FstInverter.GetSizes) to allocate memory in test code.
+template <typename Ptr, typename I>
+struct Array2Storage {
+  using ValueType = typename Array2<Ptr, I>::ValueType;
+  explicit Array2Storage(const Array2Size<I> &array2_size, int32_t stride)
+      : indexes_storage_(new I[array2_size.size1 + 1]),
+        data_storage_(new ValueType[array2_size.size2 * stride]) {
+    array2_size_ = array2_size;
+    array2_.size = array2_size_.size1;
+    array2_.indexes = indexes_storage_.get();
+    array2_.data = DataPtrCreator<Ptr, I>::Create(data_storage_, stride);
+  }
+
+  void FillIndexes(const std::vector<I> &indexes) {
+    CHECK_EQ(indexes.size(), array2_size_.size1 + 1);
+    for (auto i = 0; i != indexes.size(); ++i) array2_.indexes[i] = indexes[i];
+  }
+
+  void FillData(const std::vector<ValueType> &data) {
+    CHECK_EQ(data.size(), array2_size_.size2);
+    for (auto i = 0; i != array2_size_.size2; ++i) array2_.data[i] = data[i];
+  }
+
+  Array2<Ptr, I> &GetArray2() { return array2_; }
+
+ private:
+  Array2<Ptr, I> array2_;
+  Array2Size<I> array2_size_;
+  std::unique_ptr<I> indexes_storage_;
+  std::unique_ptr<ValueType> data_storage_;
+};
+
 }  // namespace k2
+
+namespace std {
+template <typename T, typename I>
+struct iterator_traits<k2::StridedPtr<T, I>> {
+  typedef T value_type;
+};
+
+}  // namespace std
 
 #endif  // K2_CSRC_ARRAY_H_
---
+++ k2/csrc/array_test.cc
@@ -0,0 +1,68 @@
+// k2/csrc/array_test.cc
+
+// Copyright (c)  2020  Xiaomi Corporation (author: Haowen Qiu)
+
+// See ../../LICENSE for clarification regarding multiple authors
+
+#include "k2/csrc/array.h"
+
+#include <iterator>
+#include <numeric>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace k2 {
+
+template <typename Ptr, typename IndexType>
+void TestArray2(int32_t stride) {
+  using ValueType = typename std::iterator_traits<Ptr>::value_type;
+  Array2Size<IndexType> array2_size = {4, 10};
+  std::vector<IndexType> indexes = {0, 3, 5, 9, 10};
+  std::vector<ValueType> data(array2_size.size2);
+  std::iota(data.begin(), data.end(), 0);
+
+  Array2Storage<Ptr, IndexType> array2_storage(array2_size, stride);
+  array2_storage.FillIndexes(indexes);
+  array2_storage.FillData(data);
+  Array2<Ptr, IndexType> &array2 = array2_storage.GetArray2();
+
+  EXPECT_EQ(array2.size, array2_size.size1);
+  EXPECT_FALSE(array2.Empty());
+
+  // test indexes
+  auto indexes_size = array2_size.size1 + 1;
+  for (auto i = 0; i != indexes_size; ++i) {
+    EXPECT_EQ(array2.indexes[i], indexes[i]);
+  }
+
+  // test operator []
+  for (auto i = 0; i != array2_size.size2; ++i) {
+    EXPECT_EQ(array2.data[i], data[i]);
+  }
+
+  // mutate data
+  for (auto i = 0; i != array2_size.size2; ++i) {
+    array2.data[i] += 1;
+  }
+
+  // test with indexes
+  for (auto i = 0; i != array2.size; ++i) {
+    auto start = array2.indexes[i];
+    auto end = array2.indexes[i + 1];
+    for (auto j = start; j != end; ++j) {
+      EXPECT_EQ(array2.data[j], data[j] + 1);
+    }
+  }
+}
+
+TEST(Array2Test, RawPointer) { TestArray2<int32_t *, int32_t>(1); }
+
+TEST(Array2Test, StridedPtr) {
+  TestArray2<StridedPtr<int32_t, int32_t>, int32_t>(2);
+}
+
+}  // namespace k2
