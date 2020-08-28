372cfa2915d7dfc5e997e9e324206360d30af828
Meixu Song <songmeixu@outlook.com>
2020-08-29 00:22:45 +0800

    add `K2_ASSERT, K2_CHECK_EQ, K2_PARANOID_ASSERT`
    and update for last review; make some trival changes

--- k2/csrc/cuda/debug.h
+++ k2/csrc/cuda/debug.h
@@ -22,18 +22,21 @@
 #include <device_launch_parameters.h>
 #include <driver_types.h>
 
-#include <cstdio>
+#include <stdio.h>
+#include <assert.h>
 
 #include "k2/csrc/cuda/arch.h"
 
 namespace k2 {
 
 /**
- * @brief If in debug, define the `K2_MAKE_ERROR_CHECK`
- *        to turn on error checking.
+ * @brief Define a macro `K2_MAKE_ERROR_CHECK` that turn on bellow macros
+ *        to make errors get checked, if NDEBUG is undefined.
+ *
+ * @details If in NDEBUG mode, all macros here would do nothing.
  */
 #if (!defined(NDEBUG) && !defined(K2_MAKE_ERROR_CHECK))
-  #define K2_MAKE_ERROR_CHECK
+#define K2_MAKE_ERROR_CHECK
 #endif
 
 /**
@@ -43,20 +46,79 @@ namespace k2 {
  * @param[in] msg an error message if exp is false
  *
  * @note `static_assert` is supported by both of host and device.
+ *
+ * @code{.cpp}
+ * K2_STATIC_ASSERT(DEFINED_SHAPE % DEFINED_X == 0);
+ * @endcode
  */
 #ifndef K2_STATIC_ASSERT
-  #define K2_STATIC_ASSERT(exp, msg) static_assert(exp, msg)
+#define K2_STATIC_ASSERT(exp, msg) static_assert(exp, msg)
+#endif
+
+/**
+ * @brief Check if the expression is true.
+ *
+ * @details Implemented by `assert`, which is supported by host and device.
+ *
+ * @param[in] exp the boolean expression that should be true
+ *
+ * @note
+ *  `assert(exp)` in device, if exp == 0, the kernel excution is halted.
+ *  If the program is run within a debugger, this triggers a breakpoint and the
+ *  debugger can be used to inspect the current state of the device.
+ *  Otherwise, each thread for which expression is equal to zero prints a
+ *  message to stderr after synchronization with the host via
+ *  `cudaDeviceSynchronize(),cudaStreamSynchronize(),cudaEventSynchronize()`.
+ *  The format of this message is as follows:
+ *
+ *  @code
+ *  <filename>:<line number>:<function>:
+ *  block: [blockId.x,blockId.x,blockIdx.z],
+ *  thread: [threadIdx.x,threadIdx.y,threadIdx.z]
+ *  Assertion `<expression>` failed.
+ *  @endcode
+ *
+ *  @code{.cpp}
+ *  K2_ASSERT(1 == 1);
+ *  @endcode
+ */
+#ifndef K2_ASSERT
+#define K2_ASSERT(exp) assert(exp)
+#endif
+
+/**
+ * @brief Check if two arguments are equal.
+ *
+ * @details Implemented by `assert`, which is supported by host and device.
+ *
+ * @param[in] a left argument to compare
+ * @param[in] b right argument to compare
+ *
+ * @note
+ *  Assertions are for debugging purposes. They can affect performance
+ *  and it is therefore recommended to disable them in production code.
+ *  `assert` can be disabled at compile time by defining the `NDEBUG`
+ *  preprocessor macro before including assert.h.
+ *  Thus, `K2_MAKE_ERROR_CHECK` is ignored here.
+ *
+ *  @code{.cpp}
+ *  K2_CHECK_EQ(1, 1);
+ *  @endcode
+ */
+#ifndef K2_CHECK_EQ
+#define K2_CHECK_EQ(a, b) assert( a == b )
 #endif
 
 /**
  * @fn
  *  __host__ __device__ __forceinline__ cudaError_t
- *  K2_CUDA_DEBUG(cudaError_t error,
- *                const char *filename,
- *                int line)
+ *  _K2CudaDebug(cudaError_t error,
+ *               const char *filename,
+ *               int line)
  *
  *
  * @brief This is a error checking function, with context information.
+ *        It's not designed to called by users, but inner macros.
  *
  * @details
  *  If K2_MAKE_ERROR_CHECK is defined and error is not cudaSuccess,
@@ -70,8 +132,12 @@ namespace k2 {
  * @param[in] filename      the source filename that the error comes from.
  * @param[in] line          the code line that the error happened.
  * @return                  Pass the input CUDA error.
+ *
+ * @code{.cpp}
+ * _K2CudaDebug(cudaGetLastError(), __FILE__, __LINE__);
+ * @endcode
  */
-__host__ __device__ __forceinline__ cudaError_t K2_CUDA_DEBUG(
+__host__ __device__ __forceinline__ cudaError_t _K2CudaDebug(
     cudaError_t error,
     const char *filename,
     int line) {
@@ -112,19 +178,27 @@ __host__ __device__ __forceinline__ cudaError_t K2_CUDA_DEBUG(
  * @param[in] e an enum type indicating CUDA errors.
  *
  * @remark
- *  Device cannot let host terminate/exit the process. And calling a
+ *  Device cannot terminate/exit the host process. And calling a
  *  `__host__ function("exit")` from a `__host__ __device__ function`
- *  is not allowed. Thus, the exit(e) is put here other than inside of
- *  `K2_CUDA_DEBUG`.
+ *  is not allowed. Thus, the exit(e) is put here, rather than inside of
+ *  `_K2CudaDebug`.
  *
  * @todo
  *  Should the error code return by this macro?
+ *
+ * @code{.cpp}
+ * K2_CUDA_CHECK_ERROR(cudaGetLastError());
+ * @gencode
  */
 #ifndef K2_CUDA_CHECK_ERROR
-  #define K2_CUDA_CHECK_ERROR(e)                                   \
-    if (k2::K2_CUDA_DEBUG((cudaError_t)(e), __FILE__, __LINE__)) { \
-      exit(e);                                                     \
-    }
+  #ifdef K2_MAKE_ERROR_CHECK
+    #define K2_CUDA_CHECK_ERROR(e)                                    \
+      if (::k2::_K2CudaDebug((cudaError_t)(e), __FILE__, __LINE__)) { \
+        exit(e);                                                      \
+      }
+  #else
+    #define K2_CUDA_CHECK_ERROR(e) ((void)0)
+  #endif
 #endif
 
 /**
@@ -147,15 +221,19 @@ __host__ __device__ __forceinline__ cudaError_t K2_CUDA_DEBUG(
  *  `K2_CUDA_KERNEL_SAFE_CALL`.
  *
  * @code{.cpp}
- * K2_CUDA_API_SAFE_CALL(CUDA_RUNTIME_API())
+ * K2_CUDA_API_SAFE_CALL(CUDA_RUNTIME_API());
  * @endcode
  */
 #ifndef K2_CUDA_API_SAFE_CALL
-  #define K2_CUDA_API_SAFE_CALL(c) \
-    do {                           \
-      cudaError_t e = (c);         \
-      K2_CUDA_CHECK_ERROR(e);      \
-    } while (0)
+  #ifdef K2_MAKE_ERROR_CHECK
+    #define K2_CUDA_API_SAFE_CALL(c) \
+      do {                           \
+        cudaError_t e = (c);         \
+        K2_CUDA_CHECK_ERROR(e);      \
+      } while (0)
+  #else
+    #define K2_CUDA_API_SAFE_CALL(c) (c)
+  #endif
 #endif
 
 /**
@@ -179,17 +257,21 @@ __host__ __device__ __forceinline__ cudaError_t K2_CUDA_DEBUG(
  *  otherwise the compiler rises a error "passed 2 arguments, but takes just 1".
  *
  * @code{.cpp}
- * K2_CUDA_KERNEL_SAFE_CALL(kernel_func<<<...>>>())
+ * K2_CUDA_KERNEL_SAFE_CALL(kernel_func<<<...>>>());
  * @endcode
  */
 #ifndef K2_CUDA_KERNEL_SAFE_CALL
-  #define K2_CUDA_KERNEL_SAFE_CALL(...)               \
-    do {                                              \
-      K2_CUDA_CHECK_ERROR(cudaGetLastError());        \
-      (__VA_ARGS__);                                  \
-      K2_CUDA_API_SAFE_CALL(cudaDeviceSynchronize()); \
-      K2_CUDA_CHECK_ERROR(cudaGetLastError());        \
-    } while (0)
+  #ifdef K2_MAKE_ERROR_CHECK
+    #define K2_CUDA_KERNEL_SAFE_CALL(...)               \
+      do {                                              \
+        K2_CUDA_CHECK_ERROR(cudaGetLastError());        \
+        (__VA_ARGS__);                                  \
+        K2_CUDA_API_SAFE_CALL(cudaDeviceSynchronize()); \
+        K2_CUDA_CHECK_ERROR(cudaGetLastError());        \
+      } while (0)
+  #else
+    #define K2_CUDA_KERNEL_SAFE_CALL(...) (__VA_ARGS__)
+  #endif
 #endif
 
 /**
@@ -197,16 +279,20 @@ __host__ __device__ __forceinline__ cudaError_t K2_CUDA_DEBUG(
  *
  * @brief Log macro for printf statements.
  *
- * @details `printf` is supported by both host and device.
- *          This Log is for debugging, the error msg is printed to the stderr.
- *          The log msg always get printed, regardless of macro
- *          `K2_MAKE_ERROR_CHECK`. Thus it should only be
- *          make used of in debugging.
+ * @details
+ *  `printf` is supported by both host and device. This Log is for debugging,
+ *  the error msg is printed to the stderr. The log msg always get printed,
+ *  regardless of macro `K2_MAKE_ERROR_CHECK`. Thus it should only be
+ *  make used of in debugging.
  *
  * @note
  *  This code is refered from:
  *  https://github.com/NVlabs/cub/blob/ \
  *  c3cceac115c072fb63df1836ff46d8c60d9eb304/cub/util_debug.cuh#L109
+ *
+ * @code{.cpp}
+ * K2_DLOG("Value is %d, string is %s ..", i, str);
+ * @endcode
  */
 #if !defined(K2_DLOG)
   #if !(defined(__clang__) && defined(__CUDA__))
@@ -217,7 +303,7 @@ __host__ __device__ __forceinline__ cudaError_t K2_CUDA_DEBUG(
         printf("[block (%d,%d,%d), thread (%d,%d,%d)]: " format, blockIdx.x,  \
                blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z, \
                __VA_ARGS__)
-#endif
+    #endif
   #else
     /**
      * A hack to implement the variadic printf for clang,
@@ -245,6 +331,51 @@ __host__ __device__ __forceinline__ cudaError_t K2_CUDA_DEBUG(
     #endif
     #pragma clang diagnostic pop
   #endif
+#endif
+
+/**
+ * @brief An more expensive asserts only checked if `K2_PARANOID` defined.
+ *
+ * @details
+ * If triggered, the info shown includes: file, line, [blockIdx, threadIdx],
+ * formated message, and the standard asserts info.
+ *
+ * @param[in]           exp     the compile-time boolean expression that must be true
+ * @param[in]           format  an error message if exp is false
+ * @param[in] \optional ...     the optional arguments for printf format.
+ *
+ * @note
+ * `assert` is supported by both of host and device.
+ *  - host: assert(false), rises a "SIGABRT" exit
+ *  - device: assert(false), device put msg into stderr and halt this thread,
+ *            but the msg won't get printed util synchronization.
+ *
+ * @remark
+ *  Assertions are for debugging purposes. They can affect performance
+ *  and it is therefore recommended to disable them in production code.
+ *  `assert` can be disabled at compile time by defining the `NDEBUG`
+ *  preprocessor macro before including assert.h.
+ *  Thus, `K2_MAKE_ERROR_CHECK` is ignored here.
+ *
+ * @code{.cpp}
+ * K2_PARANOID_ASSERT(a >= b, "a must be greater than b, "
+ *     "but now a = %d, b = %d", a, b);
+ *
+ * K2_PARANOID_ASSERT(a >= b, "a must be greater than b");
+ * @endcode
+ */
+#ifdef K2_PARANOID
+  #define K2_PARANOID_ASSERT(exp, format, ...)                           \
+    do {                                                                 \
+      if (exp)                                                           \
+        (void)0;                                                         \
+      else {                                                             \
+        K2_DLOG(" [%s, %d] " format, __FILE__, __LINE__, ##__VA_ARGS__); \
+        assert(exp);                                                     \
+      }                                                                  \
+    } while (0)
+#else
+  #define K2_PARANOID_ASSERT(exp, format, ...) ((void)0)
 #endif
 
 }  // namespace k2
--- k2/csrc/cuda/debug_test.cu
+++ k2/csrc/cuda/debug_test.cu
@@ -4,7 +4,12 @@
 
 // See ../../LICENSE for clarification regarding multiple authors
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#ifndef K2_PARANOID
+#define K2_PARANOID
+#endif
 
 #include "k2/csrc/cuda/debug.h"
 
@@ -16,7 +21,7 @@ __global__ void FillContents(int n, int *output) {
 
   for (int i = correct_index; i < n; i++) {
     output[i] = (i - correct_index) * 2;
-    K2_DLOG("Print from async cuda-api, output[%d] = %d", i, output[i]);
+    K2_DLOG("Print from async cuda kernel, output[%d] = %d\n", i, output[i]);
   }
 }
 
@@ -50,12 +55,31 @@ __global__ void HelloCUDA(float f) {
     K2_DLOG("Hello thread %d, f=%f\n", threadIdx.x, f);
 }
 
+// A vector add kernel definition
+__global__ void VecAdd(float* A, float* B, float* C) {
+  int i = threadIdx.x;
+  C[i] = A[i] + B[i];
+  K2_PARANOID_ASSERT(C[i] == A[i] + B[i],
+                     "Error: C[%d] is %f != A[%d] (%f) + B[%d] (%f)\n",
+                     i, C[i], i, B[i], i, B[i]);
+}
+
 TEST(DebugTest, StaticAssert) {
   K2_STATIC_ASSERT(9 % 3 == 0, "The value is unexpected");
 }
 
+TEST(DebugTest, K2Assert) {
+  K2_ASSERT(2 > 1);
+  ASSERT_DEATH(K2_ASSERT(2 < 1), "");
+}
+
+TEST(DebugTest, K2CheckEq) {
+  K2_CHECK_EQ(2 + 3, 5);
+}
+
 TEST(DebugTest, K2CudaCheckError) {
-  K2_CUDA_CHECK_ERROR(cudaSuccess);
+  ASSERT_DEATH(K2_CUDA_CHECK_ERROR(cudaErrorMemoryAllocation),
+               "cudaErrorMemoryAllocation");
 }
 
 TEST(DebugTest, K2CudaSafeCall) {
@@ -82,10 +106,11 @@ TEST(DebugTest, K2CudaSafeCall) {
   // host call K2_DLOG
   {
     /* print out 100 evenly spaced square roots just to see that it worked */
-    unsigned int i;
-    for (i = 0; i < N; i += (N / 100)) {
-      K2_DLOG("sqrt(%d) = %lf\n", i, roots[i]);
-    }
+    unsigned int i = 100000;
+    testing::internal::CaptureStdout();
+    K2_DLOG("sqrt(%d) = %lf\n", i, roots[i]);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "sqrt(100000) = 316.227766\n");
   }
 
   /* free the CPU memory */
@@ -93,11 +118,72 @@ TEST(DebugTest, K2CudaSafeCall) {
 }
 
 TEST(DebugTest, K2DLog) {
-  int *unallocated_array = static_cast<int *>(malloc(30 * sizeof(int)));
+  int *h_A = static_cast<int *>(malloc(6 * sizeof(int)));
+  int *d_A;
+  cudaMalloc(&d_A, sizeof(h_A));
+
   // device call K2_DLOG
   {
     HelloCUDA<<<1, 5>>>(1.2345f);
-    FillContents<<<2, 3>>>(5, unallocated_array);
+    FillContents<<<3, 2>>>(1, d_A);
+    cudaDeviceSynchronize();
+    K2_CUDA_CHECK_ERROR(cudaGetLastError());
+  }
+
+  cudaFree(d_A);
+  free(h_A);
+}
+
+TEST(DebugTest, K2ParanoidAssert) {
+  {
+    ASSERT_DEATH(
+        K2_PARANOID_ASSERT(2 < 1, "2 unexpectedly smaller than 1\n"),
+        "Assertion");
+  }
+
+  {
+    int a = 2;
+    int b = 1;
+    ASSERT_DEATH(
+        K2_PARANOID_ASSERT(a < b, "%d unexpectedly smaller than %d\n", a, b),
+        "Assertion");
+  }
+
+  {
+    int nn = 6;
+    size_t size = nn * sizeof(float);
+
+    // Allocate input vectors h_A and h_B in host memory
+    float h_A[6] = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5};
+    float h_B[6] = {0.5, 0.4, 0.3, 0.2, 0.1, 0.0};
+    float h_C[6];
+
+    float *d_A;
+    float *d_B;
+    float *d_C;
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+    // Copy vectors from host memory to device memory
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    // call K2_PARANOID_ASSERT in kernel
+    VecAdd<<<1, 3>>>(d_A, d_B, d_C);
+
+    // Copy result from device memory to host memory
+    // h_C contains the result in host memory
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    // call K2_PARANOID_ASSERT in host
+    K2_PARANOID_ASSERT(h_C[2] == 0.5 && h_C[3] == 0,
+                       "h_C[%d] is %f, not 0.6\n", 3, h_C[3]);
+    EXPECT_EQ(h_C[2], 0.5);
+    EXPECT_EQ(h_C[3], 0.0);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
   }
 }
 
