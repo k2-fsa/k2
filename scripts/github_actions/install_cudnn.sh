5dc8b34fac4f4faff225da0a0f4209a635b6db9f
Fangjun Kuang <csukuangfj@gmail.com>
2020-11-10 21:43:55 +0800

    rebase.

--- scripts/github_actions/install_cudnn.sh
+++ scripts/github_actions/install_cudnn.sh
@@ -4,13 +4,13 @@
 
 case $cuda in
   10.0)
-    gdown -q --id "1-Yy-X5oFJKwS-iv3vanHor8SsbYGPmH-" --output cudnn-10.0-linux-x64-v7.6.5.32.tgz
+    gdown -q --id "1H1MavvYdQosuhoTtjMpFOWUO1MiBMvC8" --output cudnn-10.0-linux-x64-v7.6.5.32.tgz
     ;;
   10.1)
-    gdown -q --id "10Xx7cQnRo_nLVZ_FOO6wdT_NlWI3I_-p" --output cudnn-10.1-linux-x64-v8.0.2.39.tgz
+    gdown -q --id "13_IVjoRJmamBVOgEGSp1Tlr5nClZUqwj" --output cudnn-10.1-linux-x64-v8.0.2.39.tgz
     ;;
   10.2)
-    gdown -q --id "1-qbuQhaZ3115c_BTPHVR7k3SqSaDtatK" --output cudnn-10.2-linux-x64-v8.0.2.39.tgz
+    gdown -q --id "1beVinj771IPuqUsQovgTh5ZMqujBzb64" --output cudnn-10.2-linux-x64-v8.0.2.39.tgz
     ;;
   *)
     echo "Unsupported cuda version: $cuda"
