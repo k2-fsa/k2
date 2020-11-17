0d20a5d68db8a7a659fda9a1ff17df0ea3086639
Fangjun Kuang <csukuangfj@gmail.com>
2020-11-18 00:16:50 +0800

    Collect k2 build information for debugging. (#356)

--- cmake/torch.cmake
+++ cmake/torch.cmake
@@ -12,3 +12,26 @@ find_package(Torch REQUIRED)
 # set the global CMAKE_CXX_FLAGS so that
 # k2 uses the same abi flag as PyTorch
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
+
+execute_process(
+  COMMAND "${PYTHON_EXECUTABLE}" -c "import torch; print(torch.__version__)"
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  OUTPUT_VARIABLE TORCH_VERSION
+)
+
+execute_process(
+  COMMAND "${PYTHON_EXECUTABLE}" -c "import torch; print(torch.version.cuda)"
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  OUTPUT_VARIABLE TORCH_CUDA_VERSION
+)
+
+message(STATUS "PyTorch version: ${TORCH_VERSION}")
+message(STATUS "PyTorch cuda version: ${TORCH_CUDA_VERSION}")
+
+if(NOT CUDA_VERSION VERSION_EQUAL TORCH_CUDA_VERSION)
+  message(FATAL_ERROR
+    "PyTorch ${TORCH_VERSION} is compiled with ${TORCH_CUDA_VERSION}.\n"
+    "But you are using ${CUDA_VERSION} to compile k2.\n"
+    "Please try to use the same cuda version for PyTorch and k2.\n"
+  )
+endif()
