915a491968f6c5cf9c1c58126db453670eeb8df7
Fangjun Kuang <csukuangfj@gmail.com>
2020-12-05 15:10:44 +0800

    support double in atomicAdd. (#455)

--- k2/csrc/tensor_ops_test.cu
+++ k2/csrc/tensor_ops_test.cu
@@ -305,11 +305,13 @@ static void TestIndexAdd2D() {
 
 TEST(IndexAdd, IndexAdd1D) {
   TestIndexAdd1D<float>();
+  TestIndexAdd1D<double>();
   TestIndexAdd1D<int32_t>();
 }
 
 TEST(IndexAdd, IndexAdd2D) {
   TestIndexAdd2D<float>();
+  TestIndexAdd2D<double>();
   TestIndexAdd2D<int32_t>();
 }
 
