5a526cf84c68d76de10c5a4729ba560b3f1e569c
Fangjun Kuang <csukuangfj@gmail.com>
2020-12-05 14:33:03 +0800

    Fix build errors for GitHub actions.

--- k2/csrc/dtype.h
+++ k2/csrc/dtype.h
@@ -173,16 +173,11 @@ struct DtypeOf<uint64_t> {
     }                                                                    \
   } while (0)
 
-#define FOR_REAL_AND_INT32_TYPES(DtypeValue, TypeName, ...)              \
+#define FOR_FLOAT_AND_INT32_TYPES(DtypeValue, TypeName, ...)             \
   do {                                                                   \
     switch (DtypeValue) {                                                \
       case kFloatDtype: {                                                \
         using TypeName = float;                                          \
-        __VA_ARGS__;                                                     \
-        break;                                                           \
-      }                                                                  \
-      case kDoubleDtype: {                                               \
-        using TypeName = double;                                         \
         __VA_ARGS__;                                                     \
         break;                                                           \
       }                                                                  \
