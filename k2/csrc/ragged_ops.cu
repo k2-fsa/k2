11831b0d8500f4d19fa2b263056ff78353945207
freewym <freewym@gmail.com>
2020-12-07 13:02:34 -0500

    change copy right info

--- k2/csrc/ragged_ops.cu
+++ k2/csrc/ragged_ops.cu
@@ -5,7 +5,7 @@
  * @copyright
  * Copyright (c)  2020  Xiaomi Corporation (authors: Daniel Povey, Haowen Qiu)
  *                      Mobvoi Inc.        (authors: Fangjun Kuang)
- *                      Microsoft Corporation (authors: Yiming Wang)
+ *                      Yiming Wang
  *
  * @copyright
  * See LICENSE for clarification regarding multiple authors
