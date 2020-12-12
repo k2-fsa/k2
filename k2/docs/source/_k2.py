e87420a487b419be1ae1b2d04125d2348b4de82f
Fangjun Kuang <csukuangfj@gmail.com>
2020-12-12 19:59:00 +0800

    fix doc building.

---
+++ k2/docs/source/_k2.py
@@ -0,0 +1,16 @@
+# this file is for generating documentation
+
+class RaggedInt: pass
+class RaggedArc: pass
+class RaggedShape: pass
+def simple_ragged_index_select(): pass
+def _as_float(): pass
+def _as_int(): pass
+def _fsa_from_str(): pass
+def _fsa_from_tensor(): pass
+def _fsa_to_str(): pass
+def _fsa_to_tensor(): pass
+def create_ragged_shape2(): pass
+def random_ragged_shape(): pass
+def _create_fsa_vec(): pass
+def _is_rand_equivalent(): pass
--- k2/docs/source/conf.py
+++ k2/docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../../python'))
 
 import sphinx_rtd_theme
 
