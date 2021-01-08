3082d106c6b80fec1c454ee7c2056476b8b5b69e
Fangjun Kuang <csukuangfj@gmail.com>
2021-01-08 15:29:50 +0800

    Use cub::device::RadixSort only when NVCC version is > 10.1.105

--- k2/csrc/ragged_ops.cu
+++ k2/csrc/ragged_ops.cu
@@ -1146,7 +1146,8 @@ Array1<int32_t> GetTransposeReordering(Ragged<int32_t> &src, int32_t num_cols) {
   K2_CHECK_EQ(device_type, kCuda);
   (void)GetTransposeReorderingThreeAxesCuda;  // remove compiler warnings
 
-#if 1
+#if (__CUDACC_VER_MAJOR__ >= 10) && (__CUDACC_VER_MINOR__ >= 1) && \
+    (__CUDACC_VER_BUILD__ > 105)
   int32_t num_buckets = num_cols;
   int32_t num_elements = src.values.Dim();
   int32_t log_buckets = static_cast<int32_t>(ceilf(log2f(num_buckets)));
