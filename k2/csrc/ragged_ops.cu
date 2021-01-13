40323a2a7f93222619386ff9d748cdf2095703d7
Daniel Povey <dpovey@gmail.com>
2021-01-13 11:16:26 +0800

    Bug fixes

--- k2/csrc/ragged_ops.cu
+++ k2/csrc/ragged_ops.cu
@@ -1162,11 +1162,13 @@ static Array1<int32_t> GetTransposeReorderingThreeAxesCuda(Ragged<int32_t> &src,
   return ans;
 }
 
-// Checks the result of GetTranspoeReordering(), and dies if it is wrong.
+// Checks the result of GetTranspoeReordering(), in debug mode and dies if it is wrong.
 static void CheckGetTransposeReordering(Ragged<int32_t> &src,
                                         Array1<int32_t> &ans) {
-  K2_CHECK(IsPermutation(ans));
-  K2_CHECK(IsMonotonic(src.values[ans]));
+  if (!internal::kDisableDebug && !internal::DisableChecks()) {
+    K2_CHECK(IsPermutation(ans));
+    K2_CHECK(IsMonotonic(src.values[ans]));
+  }
 }
 
 Array1<int32_t> GetTransposeReordering(Ragged<int32_t> &src, int32_t num_cols) {
@@ -1214,14 +1216,13 @@ Array1<int32_t> GetTransposeReordering(Ragged<int32_t> &src, int32_t num_cols) {
       reinterpret_cast<int32_t *>(d_temp_storage.Data()), ans.Data(),
       ans.Data(), num_elements, 0, log_buckets, stream));
 
-  if (!kDisableDebug && !DisableChecks())
-    CheckGetTransposeReordering(src, ans);
+  CheckGetTransposeReordering(src, ans);
   return ans;
 #else
   if (src.NumAxes() == 3) {
     Array1<int3_t> ans = GetTransposeReorderingThreeAxesCuda(src, num_cols);
-    if (!kDisableDebug && !DisableChecks())
-      CheckGetTransposeReordering(src, ans);
+    CheckGetTransposeReordering(src, ans);
+    return ans;
   }
 
   const int32_t *row_splits1_data = src.RowSplits(src.NumAxes() - 1).Data();
@@ -1253,8 +1254,7 @@ Array1<int32_t> GetTransposeReordering(Ragged<int32_t> &src, int32_t num_cols) {
   mgpu::context_t *mgpu_context = GetModernGpuAllocator(context);
 
   K2_CUDA_SAFE_CALL(mgpu::mergesort(ans.Data(), n, lambda_comp, *mgpu_context));
-  if (!kDisableDebug && !DisableChecks())
-    CheckGetTransposeReordering(src, ans);
+  CheckGetTransposeReordering(src, ans);
   return ans;
 #endif
 }
