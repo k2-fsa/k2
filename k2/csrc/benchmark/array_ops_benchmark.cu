39e080d546707de72f7db49a276c65da0976760a
Haowen <qindazhu@gmail.com>
2021-01-29 12:39:52 +0800

    using pattern trasform_scan to merge two kenerls into one

--- k2/csrc/benchmark/array_ops_benchmark.cu
+++ k2/csrc/benchmark/array_ops_benchmark.cu
@@ -9,6 +9,7 @@
 #include "k2/csrc/array_ops.h"
 #include "k2/csrc/benchmark/benchmark.h"
 #include "k2/csrc/test_utils.h"
+#include "moderngpu/kernel_scan.hxx"
 
 namespace k2 {
 
@@ -268,6 +269,60 @@ static BenchmarkStat BenchmarkSizesToMergeMap(int32_t num_src,
   return stat;
 }
 
+void TransExclusiveSumOld(const Array1<int32_t> src, Array1<int32_t> *ans) {
+  ContextPtr c = src.Context();
+  int32_t dim = src.Dim();
+  K2_CHECK_EQ(dim + 1, ans->Dim());
+  const int32_t *src_data = src.Data();
+  int32_t *ans_data = ans->Data();
+  K2_EVAL(
+      c, dim, lambda_multiple2,
+      (int32_t i)->void { ans_data[i] = src_data[i] * 2; });
+  ExclusiveSum(*ans, ans);
+}
+
+void TransExclusiveSumNew(const Array1<int32_t> src, Array1<int32_t> *ans) {
+  ContextPtr c = src.Context();
+  int32_t dim = src.Dim();
+  K2_CHECK_EQ(dim + 1, ans->Dim());
+  const int32_t *src_data = src.Data();
+  int32_t *ans_data = ans->Data();
+  K2_TRANS_EXCSUM(
+      c, dim, ans_data, lambda_multiple2,
+      (int32_t i)->int32_t { return src_data[i] * 2; });
+}
+
+static BenchmarkStat BenchmarkTransExclusiveSum(int32_t dim,
+                                                DeviceType device_type) {
+  ContextPtr context;
+  if (device_type == kCpu) {
+    context = GetCpuContext();
+  } else {
+    K2_CHECK_EQ(device_type, kCuda);
+    context = GetCudaContext();
+  }
+
+  std::vector<int32_t> vec(dim);
+  std::iota(vec.begin(), vec.end(), dim);
+  Array1<int32_t> src(context, vec);
+  Array1<int32_t> ans(context, src.Dim() + 1);
+
+  BenchmarkStat stat;
+  stat.op_name = "TransExclusiveSum_New_" + std::to_string(dim);
+  int32_t num_iter = 20;
+  stat.num_iter = num_iter;
+  stat.problem_size = dim;
+  stat.device_type = device_type;
+
+  stat.eplased_per_iter =
+      BenchmarkOp(num_iter, context,
+                  (void (*)(const Array1<int32_t> &, Array1<int32_t> *))(
+                      &TransExclusiveSumNew),
+                  src, &ans);
+  stat.eplased_per_iter *= 1e6;  // from seconds to microseconds
+  return stat;
+}
+
 template <typename T>
 static void RegisterBenchmarkExclusiveSum(DeviceType device_type) {
   std::vector<int32_t> problems_sizes = {100,  500,   1000,  2000,
@@ -351,6 +406,20 @@ static void RegisterBenchmarkSizesToMergeMap(DeviceType device_type) {
   }
 }
 
+static void RegisterBenchmarkTransExclusiveSum(DeviceType device_type) {
+  std::vector<int32_t> problems_sizes = {100,    200,     500,    1000,  2000,
+                                         5000,   10000,   20000,  50000, 100000,
+                                         500000, 1000000, 5000000};
+  for (auto s : problems_sizes) {
+    std::string name =
+        GenerateBenchmarkName<int32_t>("TransExclusiveSum", device_type) + "_" +
+        std::to_string(s);
+    RegisterBenchmark(name, [s, device_type]() -> BenchmarkStat {
+      return BenchmarkTransExclusiveSum(s, device_type);
+    });
+  }
+}
+
 static void RunArrayOpsBenchmark() {
   PrintEnvironmentInfo();
 
@@ -368,6 +437,8 @@ static void RunArrayOpsBenchmark() {
   RegisterBenchmarkSpliceRowSplits(kCuda);
 
   RegisterBenchmarkSizesToMergeMap(kCuda);
+
+  RegisterBenchmarkTransExclusiveSum(kCuda);
 
   // Users can set a regular expression via environment
   // variable `K2_BENCHMARK_FILTER` such that only benchmarks
