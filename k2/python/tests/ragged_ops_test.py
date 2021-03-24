ee5ae879507c8e012120dff85c0300649409bb07
Daniel Povey <dpovey@gmail.com>
2021-03-24 13:29:59 +0800

    Fix to comments..

--- k2/python/tests/ragged_ops_test.py
+++ k2/python/tests/ragged_ops_test.py
@@ -471,8 +471,7 @@ class TestRaggedOps(unittest.TestCase):
             src = k2.RaggedFloat(
                 '[[1 3 -1 -2] [1 0 -1] [3 2 1] [] [1] [2 3]]').to(device)
             indexes = k2.ragged.max_per_sublist(src, initial_value=0)
-
-            # -1 for an empty sublist
+            # 0 for an empty sublist
             expected = torch.tensor([3, 1, 3, 0, 1, 3], device=device)
             assert torch.all(torch.eq(indexes, expected))
 
@@ -481,7 +480,7 @@ class TestRaggedOps(unittest.TestCase):
             src = k2.RaggedFloat(
                 '[ [[3 2 1] [0 -1] []] [[2 5 3] [1 10 9 8]] ]').to(device)
             indexes = k2.ragged.max_per_sublist(src, initial_value=0)
-            # -1 for an empty sublist
+            # 0 for an empty sublist
             expected = torch.tensor([3, 0, 0, 5, 10], device=device)
             assert torch.all(torch.eq(indexes, expected))
 
