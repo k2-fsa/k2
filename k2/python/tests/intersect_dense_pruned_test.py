56e551fad15c5dfa19ee0229122e14605ef487f8
Fangjun Kuang <csukuangfj@gmail.com>
2021-05-28 19:45:55 +0800

    Fix multi-gpu for pruned intersect.

--- k2/python/tests/intersect_dense_pruned_test.py
+++ k2/python/tests/intersect_dense_pruned_test.py
@@ -34,8 +34,6 @@ class TestIntersectDensePruned(unittest.TestCase):
             3
         '''
         for device in self.devices:
-            print('FIXME. It will get stuck')
-            continue
             fsa = k2.Fsa.from_str(s).to(device)
             fsa.requires_grad_(True)
             fsa_vec = k2.create_fsa_vec([fsa])
@@ -88,52 +86,57 @@ class TestIntersectDensePruned(unittest.TestCase):
             2 3 -1 3.0
             3
         '''
-        fsa = k2.Fsa.from_str(s)
-        fsa.requires_grad_(True)
-        fsa_vec = k2.create_fsa_vec([fsa])
-        log_prob = torch.tensor(
-            [[[0.1, 0.2, 0.3], [0.04, 0.05, 0.06], [0.0, 0.0, 0.0]],
-             [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.0, 0.0, 0.0]]],
-            dtype=torch.float32,
-            requires_grad=True)
+        for device in self.devices:
+            fsa = k2.Fsa.from_str(s).to(device)
+            fsa.requires_grad_(True)
+            fsa_vec = k2.create_fsa_vec([fsa])
+            log_prob = torch.tensor(
+                [[[0.1, 0.2, 0.3], [0.04, 0.05, 0.06], [0.0, 0.0, 0.0]],
+                 [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.0, 0.0, 0.0]]],
+                dtype=torch.float32,
+                device=device,
+                requires_grad=True)
 
-        supervision_segments = torch.tensor([[0, 0, 2], [1, 0, 3]],
-                                            dtype=torch.int32)
-        dense_fsa_vec = k2.DenseFsaVec(log_prob, supervision_segments)
-        out_fsa = k2.intersect_dense_pruned(fsa_vec,
-                                            dense_fsa_vec,
-                                            search_beam=100000,
-                                            output_beam=100000,
-                                            min_active_states=0,
-                                            max_active_states=10000,
-                                            seqframe_idx_name='seqframe',
-                                            frame_idx_name='frame')
-        assert torch.allclose(
-            out_fsa.seqframe,
-            torch.tensor([0, 1, 2, 3, 4, 5, 6], dtype=torch.int32))
+            supervision_segments = torch.tensor([[0, 0, 2], [1, 0, 3]],
+                                                dtype=torch.int32)
+            dense_fsa_vec = k2.DenseFsaVec(log_prob, supervision_segments)
+            out_fsa = k2.intersect_dense_pruned(fsa_vec,
+                                                dense_fsa_vec,
+                                                search_beam=100000,
+                                                output_beam=100000,
+                                                min_active_states=0,
+                                                max_active_states=10000,
+                                                seqframe_idx_name='seqframe',
+                                                frame_idx_name='frame')
+            assert torch.all(
+                torch.eq(out_fsa.seqframe,
+                         torch.tensor([0, 1, 2, 3, 4, 5, 6], device=device)))
 
-        assert torch.allclose(
-            out_fsa.frame,
-            torch.tensor([0, 1, 2, 0, 1, 2, 3], dtype=torch.int32))
-        assert out_fsa.shape == (2, None, None), 'There should be two FSAs!'
+            assert torch.all(
+                torch.eq(out_fsa.frame,
+                         torch.tensor([0, 1, 2, 0, 1, 2, 3], device=device)))
 
-        scores = out_fsa.get_tot_scores(log_semiring=False,
-                                        use_double_scores=False)
-        scores.sum().backward()
+            assert out_fsa.shape == (2, None,
+                                     None), 'There should be two FSAs!'
 
-        # `expected` results are computed using gtn.
-        # See https://bit.ly/3oYObeb
-        expected_scores_out_fsa = torch.tensor(
-            [1.2, 2.06, 3.0, 1.2, 50.5, 2.0, 3.0])
-        expected_grad_fsa = torch.tensor([2.0, 1.0, 2.0, 2.0])
-        expected_grad_log_prob = torch.tensor([
-            0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0, 0, 0, 0.0, 1.0, 0.0, 0.0, 1.0,
-            0.0, 0.0, 0.0, 1.0
-        ]).reshape_as(log_prob)
+            scores = out_fsa.get_tot_scores(log_semiring=False,
+                                            use_double_scores=False)
+            scores.sum().backward()
 
-        assert torch.allclose(out_fsa.scores, expected_scores_out_fsa)
-        assert torch.allclose(expected_grad_fsa, fsa.scores.grad)
-        assert torch.allclose(expected_grad_log_prob, log_prob.grad)
+            # `expected` results are computed using gtn.
+            # See https://bit.ly/3oYObeb
+            expected_scores_out_fsa = torch.tensor(
+                [1.2, 2.06, 3.0, 1.2, 50.5, 2.0, 3.0], device=device)
+            expected_grad_fsa = torch.tensor([2.0, 1.0, 2.0, 2.0],
+                                             device=device)
+            expected_grad_log_prob = torch.tensor([
+                0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0, 0, 0, 0.0, 1.0, 0.0, 0.0, 1.0,
+                0.0, 0.0, 0.0, 1.0
+            ]).reshape_as(log_prob).to(device)
+
+            assert torch.allclose(out_fsa.scores, expected_scores_out_fsa)
+            assert torch.allclose(expected_grad_fsa, fsa.scores.grad)
+            assert torch.allclose(expected_grad_log_prob, log_prob.grad)
 
     def test_two_fsas(self):
         s1 = '''
@@ -150,61 +153,65 @@ class TestIntersectDensePruned(unittest.TestCase):
             2 3 -1 3.0
             3
         '''
+        for device in self.devices:
+            fsa1 = k2.Fsa.from_str(s1).to(device)
+            fsa2 = k2.Fsa.from_str(s2).to(device)
 
-        fsa1 = k2.Fsa.from_str(s1)
-        fsa2 = k2.Fsa.from_str(s2)
+            fsa1.requires_grad_(True)
+            fsa2.requires_grad_(True)
 
-        fsa1.requires_grad_(True)
-        fsa2.requires_grad_(True)
+            fsa_vec = k2.create_fsa_vec([fsa1, fsa2])
 
-        fsa_vec = k2.create_fsa_vec([fsa1, fsa2])
+            log_prob = torch.tensor(
+                [[[0.1, 0.2, 0.3], [0.04, 0.05, 0.06], [0.0, 0.0, 0.0]],
+                 [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.0, 0.0, 0.0]]],
+                dtype=torch.float32,
+                device=device,
+                requires_grad=True)
 
-        log_prob = torch.tensor(
-            [[[0.1, 0.2, 0.3], [0.04, 0.05, 0.06], [0.0, 0.0, 0.0]],
-             [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.0, 0.0, 0.0]]],
-            dtype=torch.float32,
-            requires_grad=True)
+            supervision_segments = torch.tensor([[0, 0, 2], [1, 0, 3]],
+                                                dtype=torch.int32)
+            dense_fsa_vec = k2.DenseFsaVec(log_prob, supervision_segments)
+            out_fsa = k2.intersect_dense_pruned(fsa_vec,
+                                                dense_fsa_vec,
+                                                search_beam=100000,
+                                                output_beam=100000,
+                                                min_active_states=0,
+                                                max_active_states=10000,
+                                                seqframe_idx_name='seqframe',
+                                                frame_idx_name='frame')
+            assert torch.all(
+                torch.eq(out_fsa.seqframe,
+                         torch.tensor([0, 1, 2, 3, 4, 5, 6], device=device)))
 
-        supervision_segments = torch.tensor([[0, 0, 2], [1, 0, 3]],
-                                            dtype=torch.int32)
-        dense_fsa_vec = k2.DenseFsaVec(log_prob, supervision_segments)
-        out_fsa = k2.intersect_dense_pruned(fsa_vec,
-                                            dense_fsa_vec,
-                                            search_beam=100000,
-                                            output_beam=100000,
-                                            min_active_states=0,
-                                            max_active_states=10000,
-                                            seqframe_idx_name='seqframe',
-                                            frame_idx_name='frame')
-        assert torch.allclose(
-            out_fsa.seqframe,
-            torch.tensor([0, 1, 2, 3, 4, 5, 6], dtype=torch.int32))
+            assert torch.all(
+                torch.eq(out_fsa.frame,
+                         torch.tensor([0, 1, 2, 0, 1, 2, 3], device=device)))
 
-        assert torch.allclose(
-            out_fsa.frame,
-            torch.tensor([0, 1, 2, 0, 1, 2, 3], dtype=torch.int32))
-        assert out_fsa.shape == (2, None, None), 'There should be two FSAs!'
+            assert out_fsa.shape == (2, None,
+                                     None), 'There should be two FSAs!'
 
-        scores = out_fsa.get_tot_scores(log_semiring=False,
-                                        use_double_scores=False)
-        scores.sum().backward()
+            scores = out_fsa.get_tot_scores(log_semiring=False,
+                                            use_double_scores=False)
+            scores.sum().backward()
 
-        # `expected` results are computed using gtn.
-        # See https://bit.ly/3oYObeb
-        expected_scores_out_fsa = torch.tensor(
-            [1.2, 2.06, 3.0, 1.2, 50.5, 2.0, 3.0])
+            # `expected` results are computed using gtn.
+            # See https://bit.ly/3oYObeb
+            expected_scores_out_fsa = torch.tensor(
+                [1.2, 2.06, 3.0, 1.2, 50.5, 2.0, 3.0], device=device)
 
-        expected_grad_fsa1 = torch.tensor([1.0, 1.0, 1.0])
-        expected_grad_fsa2 = torch.tensor([1.0, 1.0, 1.0, 1.0])
-        expected_grad_log_prob = torch.tensor([
-            0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0, 0, 0, 0.0, 1.0, 0.0, 0.0, 1.0,
-            0.0, 0.0, 0.0, 1.0
-        ]).reshape_as(log_prob)
+            expected_grad_fsa1 = torch.tensor([1.0, 1.0, 1.0], device=device)
+            expected_grad_fsa2 = torch.tensor([1.0, 1.0, 1.0, 1.0],
+                                              device=device)
+            expected_grad_log_prob = torch.tensor([
+                0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0, 0, 0, 0.0, 1.0, 0.0, 0.0, 1.0,
+                0.0, 0.0, 0.0, 1.0
+            ]).reshape_as(log_prob).to(device)
 
-        assert torch.allclose(out_fsa.scores, expected_scores_out_fsa)
-        assert torch.allclose(expected_grad_fsa1, fsa1.scores.grad)
-        assert torch.allclose(expected_grad_fsa2, fsa2.scores.grad)
-        assert torch.allclose(expected_grad_log_prob, log_prob.grad)
+            assert torch.allclose(out_fsa.scores, expected_scores_out_fsa)
+            assert torch.allclose(expected_grad_fsa1, fsa1.scores.grad)
+            assert torch.allclose(expected_grad_fsa2, fsa2.scores.grad)
+            assert torch.allclose(expected_grad_log_prob, log_prob.grad)
 
     def test_two_fsas_long_pruned(self):
         # as test_two_fsas_long in intersect_dense_test.py,
@@ -224,8 +231,6 @@ class TestIntersectDensePruned(unittest.TestCase):
             3
         '''
         for device in self.devices:
-            print('FIXME. It will get stuck')
-            continue
             fsa1 = k2.Fsa.from_str(s1)
             fsa2 = k2.Fsa.from_str(s2)
 
