8ddeb091d4a9f58e743eb135cbf5a84ae6f17798
Fangjun Kuang <csukuangfj@gmail.com>
2021-10-04 08:49:53 +0800

    Test jit module with faked data.
    I have compared the output from C++ with that from Python.
    The sums of the tensors are equal.

--- k2/torch/bin/decode.cc
+++ k2/torch/bin/decode.cc
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include "torch/script.h"
 
 C10_DEFINE_string(jit_pt, "", "Path to exported jit filename.");
@@ -10,17 +12,40 @@ int main(int argc, char *argv[]) {
   torch::SetUsageMessage(usage);
 
   torch::ParseCommandLineFlags(&argc, &argv);
+  if (FLAGS_jit_pt.empty()) {
+    std::cout << "Please provide --jit_pt"
+              << "\n";
+    std::cout << torch::UsageMessage() << "\n";
+    return -1;
+  }
+
   std::cout << "jit pt filename: " << FLAGS_jit_pt << "\n";
 
   torch::jit::script::Module module;
   module = torch::jit::load(FLAGS_jit_pt);
   module.eval();
-  std::cout << "is training: " << module.is_training() << "\n";
+  torch::Tensor x = torch::ones({1, 100, 80}, torch::kFloat32);
+
+  std::vector<torch::jit::IValue> inputs;
+  inputs.emplace_back(std::move(x));
+
+  // the output for module.forward() is a tuple of 3 tensors
+  auto outputs = module.forward(inputs).toTuple();
+  assert(outputs->elements().size() == 3u);
+
+  auto nnet_output = outputs->elements()[0].toTensor();
+  auto memory = outputs->elements()[1].toTensor();
+  assert(outputs->elements()[2].isNone() == true);
+
+  std::cout << nnet_output.sum() << "\n";
+  std::cout << memory.sum() << "\n";
 
   // clang-format off
   // TODO(fangjun):
-  // (1) Use faked data to test "module" and compare it outputs with that from Python
-  // (2) Use some third party library to read wave files (or write our own)
-  // (3) Use kaldifeat to compute features
+  // [x] Use faked data to test "module" and compare its outputs with that from Python
+  // [ ] Use some third party library to read wave files (or write our own)
+  // [ ] Use kaldifeat to compute features
+  // [ ] Inplement CTC decoding
+  // [ ] Inplement HLG decoding
   // clang-format on
 }
