cf70b3021fcc08519064b7c18f2ef41d5cc996ee
Fangjun Kuang <csukuangfj@gmail.com>
2021-10-10 22:43:27 +0800

    Implement HLG decoding.

--- k2/torch/bin/decode.cu
+++ k2/torch/bin/decode.cu
@@ -11,6 +11,7 @@
 #include "k2/csrc/ragged_ops.h"
 #include "k2/torch/csrc/dense_fsa_vec.h"
 #include "k2/torch/csrc/deserialization.h"
+#include "k2/torch/csrc/symbol_table.h"
 #include "k2/torch/csrc/utils.h"
 #include "sentencepiece_processor.h"
 #include "torch/script.h"
@@ -18,8 +19,21 @@
 
 C10_DEFINE_string(jit_pt, "", "Path to exported jit filename.");
 C10_DEFINE_string(feature_pt, "", "Path to pre-computed feature filename.");
-C10_DEFINE_string(bpe_model, "", "Path to a pretrained BPE model.");
-C10_DEFINE_string(hlg, "", "Path to HLG.pt.");
+C10_DEFINE_string(
+    bpe_model, "",
+    "Path to a pretrained BPE model. Needed if --use_ctc_decoding is true");
+C10_DEFINE_bool(use_ctc_decoding, true, "True to use CTC decoding");
+C10_DEFINE_string(hlg, "",
+                  "Path to HLG.pt. Needed if --use_ctc_decoding is false");
+C10_DEFINE_string(word_table, "",
+                  "Path to wors.txt. Needed if --use_ctc_decoding is false");
+//
+C10_DEFINE_double(search_beam, 20, "search_beam in IntersectDensePruned");
+C10_DEFINE_double(output_beam, 8, "output_beam in IntersectDensePruned");
+C10_DEFINE_int(min_activate_states, 30,
+               "min_activate_states in IntersectDensePruned");
+C10_DEFINE_int(max_activate_states, 10000,
+               "max_activate_states in IntersectDensePruned");
 // TODO: add more options
 
 // Load a file saved by torch.save(x, filename), where
@@ -70,34 +84,54 @@ static torch::Tensor GetSupervisionSegments(torch::IValue supervisions,
   return supervision_segments;
 }
 
-static k2::FsaVec CtcDecoding(k2::FsaVec &ctc_topo,
-                              k2::DenseFsaVec &dense_fsa_vec,
-                              k2::Array1<int32_t> &in_aux_labels,
-                              k2::Array1<int32_t> *out_aux_labels) {
-  K2_CHECK_NE(out_aux_labels, nullptr);
+static k2::FsaVec GetLattice(
+    torch::Tensor nnet_output, k2::FsaVec decoding_graph,
+    torch::Tensor supervision_segments, float search_beam, float output_beam,
+    int32_t min_activate_states, int32_t max_activate_states,
+    int32_t subsampling_factor, k2::Array1<int32_t> &in_aux_labels,
+    k2::Ragged<int32_t> &in_ragged_aux_labels,
+    k2::Array1<int32_t> *out_aux_labels,
+    k2::Ragged<int32_t> *out_ragged_aux_labels) {
+  if (in_aux_labels.Dim() != 0) {
+    K2_CHECK_EQ(in_ragged_aux_labels.values.Dim(), 0);
+  } else {
+    K2_CHECK_NE(in_ragged_aux_labels.values.Dim(), 0);
+  }
 
-  float search_beam = 20;
-  float output_beam = 8;
-  int32_t min_activate_states = 30;
-  int32_t max_activate_states = 10000;
+  k2::DenseFsaVec dense_fsa_vec = k2::CreateDenseFsaVec(
+      nnet_output, supervision_segments, subsampling_factor - 1);
+
   k2::FsaVec lattice;
   k2::Array1<int32_t> arc_map_a;
   k2::Array1<int32_t> arc_map_b;
-  k2::IntersectDensePruned(ctc_topo, dense_fsa_vec, search_beam, output_beam,
-                           min_activate_states, max_activate_states, &lattice,
-                           &arc_map_a, &arc_map_b);
-
-  // see Index() in array_ops.h
-  *out_aux_labels =
-      k2::Index(in_aux_labels, arc_map_a, /*allow_minus_one*/ false,
-                /*default_value*/ 0);
-
+  k2::IntersectDensePruned(decoding_graph, dense_fsa_vec, search_beam,
+                           output_beam, min_activate_states,
+                           max_activate_states, &lattice, &arc_map_a,
+                           &arc_map_b);
+  if (in_aux_labels.Dim() > 0) {
+    // see Index() in array_ops.h
+    *out_aux_labels =
+        k2::Index(in_aux_labels, arc_map_a, /*allow_minus_one*/ false,
+                  /*default_value*/ 0);
+  } else {
+    // See Index() in ragged_ops.h
+    *out_ragged_aux_labels =
+        k2::Index(in_ragged_aux_labels, /*axis*/ 0, arc_map_a);
+  }
   return lattice;
 }
 
-static std::vector<std::string> GetTexts(
-    k2::FsaVec &lattice, k2::Array1<int32_t> &aux_labels,
-    sentencepiece::SentencePieceProcessor &processor) {
+static k2::FsaVec OneBestDecoding(k2::FsaVec &lattice,
+                                  k2::Array1<int32_t> &in_aux_labels,
+                                  k2::Ragged<int32_t> &in_ragged_aux_labels,
+                                  k2::Array1<int32_t> *out_aux_labels,
+                                  k2::Ragged<int32_t> *out_ragged_aux_labels) {
+  if (in_aux_labels.Dim() != 0) {
+    K2_CHECK_EQ(in_ragged_aux_labels.values.Dim(), 0);
+  } else {
+    K2_CHECK_NE(in_ragged_aux_labels.values.Dim(), 0);
+  }
+
   k2::Ragged<int32_t> state_batches = k2::GetStateBatches(lattice, true);
   k2::Array1<int32_t> dest_states = k2::GetDestStates(lattice, true);
   k2::Ragged<int32_t> incoming_arcs = k2::GetIncomingArcs(lattice, dest_states);
@@ -112,20 +146,43 @@ static std::vector<std::string> GetTexts(
   k2::Ragged<int32_t> best_path_arc_indexes =
       k2::ShortestPath(lattice, entering_arcs);
 
-  // See Index() in ragged_ops.h
-  k2::Ragged<int32_t> ragged_aux_labels =
-      k2::Index(aux_labels, best_path_arc_indexes);
-  ragged_aux_labels = k2::RemoveValuesLeq(ragged_aux_labels, 0);
-
-  std::vector<std::vector<int32_t>> aux_labels_vec =
-      ragged_aux_labels.ToVecVec();
-  std::vector<std::string> ans;
-  for (const auto &ids : aux_labels_vec) {
-    std::string text;
-    processor.Decode(ids, &text);
-    ans.emplace_back(std::move(text));
+  if (in_aux_labels.Dim() > 0) {
+    *out_aux_labels = k2::Index(in_aux_labels, best_path_arc_indexes.values,
+                                /*allow_minus_one*/ false,
+                                /*default_value*/ 0);
+  } else {
+    *out_ragged_aux_labels = k2::Index(in_ragged_aux_labels, /*axis*/ 0,
+                                       best_path_arc_indexes.values);
   }
-  return ans;
+
+  k2::FsaVec out = k2::FsaVecFromArcIndexes(lattice, best_path_arc_indexes);
+  return out;
+}
+
+static k2::Ragged<int32_t> GetTexts(k2::FsaVec &lattice,
+                                    k2::Array1<int32_t> &in_aux_labels,
+                                    k2::Ragged<int32_t> &in_ragged_aux_labels) {
+  if (in_aux_labels.Dim() != 0) {
+    K2_CHECK_EQ(in_ragged_aux_labels.values.Dim(), 0);
+  } else {
+    K2_CHECK_NE(in_ragged_aux_labels.values.Dim(), 0);
+  }
+
+  k2::Ragged<int32_t> ragged_aux_labels;
+  if (in_aux_labels.Dim() != 0) {
+    // [utt][state][arc] -> [utt][arc]
+    k2::RaggedShape aux_labels_shape = k2::RemoveAxis(lattice.shape, 1);
+    ragged_aux_labels = k2::Ragged<int32_t>(aux_labels_shape, in_aux_labels);
+  } else {
+    k2::RaggedShape aux_labels_shape =
+        k2::ComposeRaggedShapes(lattice.shape, in_ragged_aux_labels.shape);
+    aux_labels_shape = k2::RemoveAxis(aux_labels_shape, 1);
+    aux_labels_shape = k2::RemoveAxis(aux_labels_shape, 1);
+    ragged_aux_labels =
+        k2::Ragged<int32_t>(aux_labels_shape, in_ragged_aux_labels.values);
+  }
+  ragged_aux_labels = k2::RemoveValuesLeq(ragged_aux_labels, 0);
+  return ragged_aux_labels;
 }
 
 int main(int argc, char *argv[]) {
@@ -138,8 +195,10 @@ int main(int argc, char *argv[]) {
     ./bin/decode \
       --jit_pt <path to exported torch script pt file> \
       --feature_pt <path to precomputed feature pt file> \
+      --use_ctc_decoding <true|false> \
       --bpe_model <path to pretrained BPE model> \
       --hlg <path to HLG.pt> \
+      --word_tabel <path to words.txt>
   )";
   torch::SetUsageMessage(usage);
 
@@ -158,15 +217,22 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  if (FLAGS_bpe_model.empty()) {
+  if (FLAGS_use_ctc_decoding && FLAGS_bpe_model.empty()) {
     std::cout << "Please provide --bpe_model"
               << "\n";
     std::cout << torch::UsageMessage() << "\n";
     return -1;
   }
 
-  if (FLAGS_hlg.empty()) {
+  if (FLAGS_use_ctc_decoding == false && FLAGS_hlg.empty()) {
     std::cout << "Please provide --hlg"
+              << "\n";
+    std::cout << torch::UsageMessage() << "\n";
+    return -1;
+  }
+
+  if (FLAGS_use_ctc_decoding == false && FLAGS_word_table.empty()) {
+    std::cout << "Please provide --word_table"
               << "\n";
     std::cout << torch::UsageMessage() << "\n";
     return -1;
@@ -209,38 +275,60 @@ int main(int argc, char *argv[]) {
   torch::Tensor supervision_segments =
       GetSupervisionSegments(supervisions, subsampling_factor);
 
-  k2::DenseFsaVec dense_fsa_vec = k2::CreateDenseFsaVec(
-      nnet_output, supervision_segments, subsampling_factor - 1);
-
   k2::ContextPtr ctx = k2::ContextFromTensor(nnet_output);
 
-  k2::Fsa lattice;
-  bool use_ctc_decoding = true;
-  k2::Array1<int32_t> aux_labels;
+  k2::Fsa decoding_graph;
 
-  if (use_ctc_decoding) {
+  k2::Array1<int32_t> aux_labels;  // only one of the two aux_labels is used
+  k2::Ragged<int32_t> ragged_aux_labels;
+
+  if (FLAGS_use_ctc_decoding) {
     k2::Fsa ctc_topo = k2::CtcTopo(ctx, nnet_output.size(2) - 1,
                                    /*modified*/ false, &aux_labels);
-    ctc_topo = k2::FsaToFsaVec(ctc_topo);
-    lattice = CtcDecoding(ctc_topo, dense_fsa_vec, aux_labels, &aux_labels);
+    decoding_graph = k2::FsaToFsaVec(ctc_topo);
   } else {
     // TODO(fangjun): We will eventually use an FSA wrapper to
     // associate attributes with an FSA.
-    k2::Ragged<int32_t> HLG_aux_labels;
-    k2::Fsa HLG = k2::LoadFsa(FLAGS_hlg, &HLG_aux_labels);
-    K2_LOG(INFO) << HLG.NumElements();
-    K2_LOG(INFO) << HLG_aux_labels.NumElements();
-    // TODO(fangjun): attach aux labels to HLG
-    // and use HLG for decoding
+    decoding_graph = k2::LoadFsa(FLAGS_hlg, &ragged_aux_labels);
+  }
+  k2::FsaVec lattice = GetLattice(
+      nnet_output, decoding_graph, supervision_segments, FLAGS_search_beam,
+      FLAGS_output_beam, FLAGS_min_activate_states, FLAGS_max_activate_states,
+      subsampling_factor, aux_labels, ragged_aux_labels, &aux_labels,
+      &ragged_aux_labels);
+
+  lattice = OneBestDecoding(lattice, aux_labels, ragged_aux_labels, &aux_labels,
+                            &ragged_aux_labels);
+
+  ragged_aux_labels = GetTexts(lattice, aux_labels, ragged_aux_labels);
+  auto aux_labels_vec = ragged_aux_labels.ToVecVec();
+
+  std::vector<std::string> texts;
+  if (FLAGS_use_ctc_decoding) {
+    sentencepiece::SentencePieceProcessor processor;
+    const auto status = processor.Load(FLAGS_bpe_model);
+    if (!status.ok()) {
+      K2_LOG(FATAL) << status.ToString();
+    }
+    for (const auto &ids : aux_labels_vec) {
+      std::string text;
+      processor.Decode(ids, &text);
+      texts.emplace_back(std::move(text));
+    }
+  } else {
+    k2::SymbolTable symbol_table(FLAGS_word_table);
+    for (const auto &ids : aux_labels_vec) {
+      std::string text;
+      std::string sep = "";
+      for (auto id : ids) {
+        text.append(sep);
+        text.append(symbol_table[id]);
+        sep = " ";
+      }
+      texts.emplace_back(std::move(text));
+    }
   }
 
-  sentencepiece::SentencePieceProcessor processor;
-  const auto status = processor.Load(FLAGS_bpe_model);
-  if (!status.ok()) {
-    K2_LOG(FATAL) << status.ToString();
-  }
-
-  auto texts = GetTexts(lattice, aux_labels, processor);
   for (const auto &text : texts) {
     K2_LOG(INFO) << text;
   }
