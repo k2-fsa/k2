df375d84afd5c205dd4d11a97aa3545c8ffed7e3
Fangjun Kuang <csukuangfj@gmail.com>
2021-11-08 17:21:18 +0800

    Refactor bin/decode.cu (#869)
    * Add CTC decode.
    
    * Add HLG decoding.
    
    * Add n-gram LM rescoring.
    
    * Remove unused files.
    
    * Fix style issues.
    
    * Add missing files.

--- cmake/sentencepiece.cmake
+++ cmake/sentencepiece.cmake
@@ -44,7 +44,7 @@ function(download_sentencepiece)
 
   add_subdirectory(${sentencepiece_SOURCE_DIR} ${sentencepiece_BINARY_DIR} EXCLUDE_FROM_ALL)
 
-  # we will link to the static library of sentencepiece
+  # Link to sentencepiece statically
   target_include_directories(sentencepiece-static
     INTERFACE
       ${sentencepiece_SOURCE_DIR}
