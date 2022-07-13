912497fbfa2409b86e80784960ae7f53b52970ec
pkufool <wkang@pku.org.cn>
2022-07-13 15:47:02 +0800

    Fix flake8

--- k2/python/tests/ragged_ops_test.py
+++ k2/python/tests/ragged_ops_test.py
@@ -115,7 +115,6 @@ class TestRaggedOps(unittest.TestCase):
             assert ans.numel() == 0
             assert ans.size() == (3, 0)
 
-
     def test_pad_float(self):
         s = '''
             [ [ 1.0 2.0 ] [ 3.0 ] [ ] [ 4.0 5.0 6.0 ] [ 7.0 8.0 9.0 10.0 ] ]
