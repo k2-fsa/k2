3db7986396d3d6a2c06e0042aecccf6636d590f5
pkufool <wkang.pku@gmail.com>
2022-07-26 23:18:04 +0800

    Build android arr

--- k2/csrc/default_context.cu
+++ k2/csrc/default_context.cu
@@ -18,6 +18,7 @@
  */
 
 #include <cstdlib>
+#include <cstring>
 #include <mutex>  // NOLINT
 
 #include "k2/csrc/context.h"
@@ -28,11 +29,9 @@ namespace k2 {
 
 static constexpr std::size_t kAlignment = 64;
 
-// TODO(haowen): most of implementations below should be updated later.
 class CpuContext : public Context {
  public:
   CpuContext() = default;
-  ContextPtr GetCpuContext() override { return shared_from_this(); }
   DeviceType GetDeviceType() const override { return kCpu; }
 
   void *Allocate(std::size_t bytes, void **deleter_context) override {
@@ -52,11 +51,19 @@ class CpuContext : public Context {
   void Deallocate(void *data, void * /*deleter_context*/) override {
     free(data);
   }
+
+  void CopyDataTo(size_t num_bytes, const void *src, ContextPtr dst_context,
+                  void *dst) override {
+    DeviceType device_type = dst_context->GetDeviceType();
+    K2_CHECK_EQ(device_type, kCpu);
+    memcpy(dst, src, num_bytes);
+  };
 };
 
 class CudaContext : public Context {
  public:
   explicit CudaContext(int32_t gpu_id) : gpu_id_(gpu_id) {
+#ifdef K2_WITH_CUDA
     if (gpu_id_ != -1) {
       auto ret = cudaSetDevice(gpu_id_);
       K2_CHECK_CUDA_ERROR(ret);
@@ -65,42 +72,60 @@ class CudaContext : public Context {
     // and handle GPU ids from multiple machines.
     auto ret = cudaStreamCreate(&stream_);
     K2_CHECK_CUDA_ERROR(ret);
+#else
+    K2_LOG(FATAL) << "Unreachable code.";
+#endif
   }
-  ContextPtr GetCpuContext() override { return k2::GetCpuContext(); }
+
   DeviceType GetDeviceType() const override { return kCuda; }
   int32_t GetDeviceId() const override { return gpu_id_; }
 
   void *Allocate(std::size_t bytes, void **deleter_context) override {
     void *p = nullptr;
+#ifdef K2_WITH_CUDA
     if (bytes) {
       auto ret = cudaMalloc(&p, bytes);
       K2_CHECK_CUDA_ERROR(ret);
     }
     if (deleter_context != nullptr) *deleter_context = nullptr;
+#endif
     return p;
   }
+
+  void CopyDataTo(size_t num_bytes, const void *src, ContextPtr dst_context,
+                  void *dst) override{};
 
   bool IsCompatible(const Context &other) const override {
     return other.GetDeviceType() == kCuda && other.GetDeviceId() == gpu_id_;
   }
 
   void Deallocate(void *data, void * /*deleter_context*/) override {
+#ifdef K2_WITH_CUDA
     auto ret = cudaFree(data);
     K2_CHECK_CUDA_ERROR(ret);
+#endif
   }
 
   cudaStream_t GetCudaStream() const override {
+#ifdef K2_WITH_CUDA
     return g_stream_override.OverrideStream(stream_);
+#else
+    return cudaStream_t{};
+#endif
   }
 
   void Sync() const override {
+#ifdef K2_WITH_CUDA
     auto ret = cudaStreamSynchronize(stream_);
     K2_CHECK_CUDA_ERROR(ret);
+#endif
   }
 
   ~CudaContext() {
+#ifdef K2_WITH_CUDA
     auto ret = cudaStreamDestroy(stream_);
     K2_CHECK_CUDA_ERROR(ret);
+#endif
   }
 
  private:
@@ -111,6 +136,7 @@ class CudaContext : public Context {
 ContextPtr GetCpuContext() { return std::make_shared<CpuContext>(); }
 
 ContextPtr GetCudaContext(int32_t gpu_id /*= -1*/) {
+#ifdef K2_WITH_CUDA
   static std::once_flag has_cuda_init_flag;
   static bool has_cuda = false;
   std::call_once(has_cuda_init_flag, []() {
@@ -123,8 +149,10 @@ ContextPtr GetCudaContext(int32_t gpu_id /*= -1*/) {
   });
 
   if (has_cuda) return std::make_shared<CudaContext>(gpu_id);
-
   return GetCpuContext();
+#else
+  return GetCpuContext();
+#endif
 }
 
 }  // namespace k2
