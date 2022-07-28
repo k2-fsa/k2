ee623309b853eb8082257fbbd402e8ceee5eed20
Fangjun Kuang <csukuangfj@gmail.com>
2022-07-28 22:22:41 +0800

    Fix building on Windows.

--- k2/python/csrc/version.cu
+++ k2/python/csrc/version.cu
@@ -27,20 +27,20 @@
 void PybindVersion(py::module &m) {
   py::module version = m.def_submodule("version", "k2 version information");
 
-  version.attr("__version__") = k2::kVersion;
-  version.attr("git_sha1") = k2::kGitSha1;
-  version.attr("git_date") = k2::kGitDate;
-  version.attr("cuda_version") = k2::kCudaVersion;
-  version.attr("cudnn_version") = k2::kCudnnVersion;
-  version.attr("python_version") = k2::kPythonVersion;
-  version.attr("build_type") = k2::kBuildType;
-  version.attr("os_type") = k2::kOS;
-  version.attr("cmake_version") = k2::kCMakeVersion;
-  version.attr("gcc_version") = k2::kGCCVersion;
-  version.attr("cmake_cuda_flags") = k2::kCMakeCudaFlags;
-  version.attr("cmake_cxx_flags") = k2::kCMakeCxxFlags;
-  version.attr("torch_version") = k2::kTorchVersion;
-  version.attr("torch_cuda_version") = k2::kTorchCudaVersion;
+  version.attr("__version__") = std::string(k2::kVersion);
+  version.attr("git_sha1") = std::string(k2::kGitSha1);
+  version.attr("git_date") = std::string(k2::kGitDate);
+  version.attr("cuda_version") = std::string(k2::kCudaVersion);
+  version.attr("cudnn_version") = std::string(k2::kCudnnVersion);
+  version.attr("python_version") = std::string(k2::kPythonVersion);
+  version.attr("build_type") = std::string(k2::kBuildType);
+  version.attr("os_type") = std::string(k2::kOS);
+  version.attr("cmake_version") = std::string(k2::kCMakeVersion);
+  version.attr("gcc_version") = std::string(k2::kGCCVersion);
+  version.attr("cmake_cuda_flags") = std::string(k2::kCMakeCudaFlags);
+  version.attr("cmake_cxx_flags") = std::string(k2::kCMakeCxxFlags);
+  version.attr("torch_version") = std::string(k2::kTorchVersion);
+  version.attr("torch_cuda_version") = std::string(k2::kTorchCudaVersion);
   version.attr("enable_nvtx") = k2::kEnableNvtx;
   version.attr("disable_debug") = k2::internal::kDisableDebug;
   version.attr("with_cuda") = k2::kWithCuda;
