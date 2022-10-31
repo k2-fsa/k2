c3a7404e0d9da4d8d99f3f15c54d80b321c1b960
Fangjun Kuang <csukuangfj@gmail.com>
2022-10-31 12:45:41 +0800

    Support torch 1.13 (#1084)
    * Support torch 1.13
    * Update pybind11 to support python 3.11

--- scripts/github_actions/generate_build_matrix.py
+++ scripts/github_actions/generate_build_matrix.py
@@ -87,16 +87,20 @@ def generate_build_matrix(enable_cuda, test_only_latest_torch):
             "python-version": ["3.7", "3.8", "3.9", "3.10"],
             "cuda": ["10.2", "11.3", "11.6"],
         },
+        "1.13.0": {
+            "python-version": ["3.7", "3.8", "3.9", "3.10", "3.11"],
+            "cuda": ["11.6", "11.7"],  # default 11.7
+        },
     }
     if test_only_latest_torch:
-        latest = "1.12.1"
+        latest = "1.13.0"
         matrix = {latest: matrix[latest]}
 
     # We only have limited spaces in anaconda, so we exclude some
     # versions of PyTorch here. If you need them, please consider
     # installing k2 from source
     # Only CUDA build are excluded since it occupies more disk space
-    excluded_torch_versions = ["1.6.0", "1.7.0", "1.7.1"]
+    excluded_torch_versions = ["1.6.0", "1.7.0", "1.7.1", "1.8.0", "1.8.1"]
 
     ans = []
     for torch, python_cuda in matrix.items():
