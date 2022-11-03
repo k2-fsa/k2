c9f7f6f53425ce39065f8c929d5c763cf4d4ad3e
Fangjun Kuang <csukuangfj@gmail.com>
2022-11-03 15:31:41 +0800

    Fix building on Windows

---
+++ scripts/github_actions/fix_torch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import torch
+from pathlib import Path
+import urllib.request
+
+
+def get_pytorch_version():
+    # if it is 1.7.1+cuda101, then strip +cuda101
+    return torch.__version__.split("+")[0]
+
+
+def fix_pytorch_1_12():
+    print("Fix https://github.com/pytorch/pytorch/issues/88290")
+
+    torch_dir = Path(torch.__file__).parent
+    print("torch_dir", torch_dir)
+    mobile_dir = torch_dir / "include" / "torch" / "csrc" / "jit" / "mobile"
+    if mobile_dir.is_dir():
+        print("Skip")
+        return
+    mobile_dir.mkdir()
+    files = (
+        "code.h",
+        "debug_info.h",
+        "function.h",
+        "method.h",
+        "module.h",
+    )
+    base_url = "https://raw.githubusercontent.com/pytorch/pytorch/v1.12.1/torch/csrc/jit/mobile/"  # noqa
+    for f in files:
+        path = mobile_dir / f
+        url = base_url + f
+        print(f"Donwloading {url} to {path}")
+        urllib.request.urlretrieve(url, path)
+
+
+def main():
+    if "1.12" in get_pytorch_version():
+        fix_pytorch_1_12()
+    else:
+        print(f"Skip since version is {get_pytorch_version()}")
+
+
+if __name__ == "__main__":
+    main()
