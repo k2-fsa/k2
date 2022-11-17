708ac362e23b534aadb3a5b8f9af441bfdcd6ce4
Fangjun Kuang <csukuangfj@gmail.com>
2022-11-17 16:56:21 +0800

    Fix installation for torch 1.12.x

--- scripts/github_actions/fix_torch.sh
+++ scripts/github_actions/fix_torch.sh
@@ -6,20 +6,20 @@ if [[ "${torch}" == "1.12.0" || ${torch} == "1.12.1" ]]; then
   torch_dir=$(python3 -c "from pathlib import Path; import torch; print(Path(torch.__file__).parent)")
   echo "torch_dir: ${torch_dir}"
   cd $torch_dir/include/torch/csrc/jit
-  if [ ! -d mobile ]; then
-    mkdir mobile
-    cd mobile
-    files=(
-      code.h
-      debug_info.h
-      function.h
-      method.h
-      module.h
-    )
-    for f in ${files[@]}; do
+  mkdir -p mobile
+  cd mobile
+  files=(
+    code.h
+    debug_info.h
+    function.h
+    method.h
+    module.h
+  )
+  for f in ${files[@]}; do
+    if [ ! -f $f ]; then
       url=https://raw.githubusercontent.com/pytorch/pytorch/v1.12.1/torch/csrc/jit/mobile/$f
       echo "Downloading $url"
       wget $url
-    done
-  fi
+    fi
+  done
 fi
