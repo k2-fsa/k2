9acf87444c3997a21b715ea6c7fca8325dfa8eb8
Fangjun Kuang <csukuangfj@gmail.com>
2023-02-28 19:00:35 +0800

    add links to pre-compiled CPU/CUDA wheels for Linux (#1166)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -190,3 +190,7 @@ def setup(app):
     app.add_css_file('custom.css')
     app.connect('autodoc-process-signature', replace_signature)
     app.connect('autodoc-process-docstring', replace_doc)
+
+rst_epilog = """
+.. _k2: https://github.com/k2-fsa/k2
+"""
