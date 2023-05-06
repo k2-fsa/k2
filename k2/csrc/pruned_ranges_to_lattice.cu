dbb590a217d3be6ad8309f03b0a015940580438b
Guo Liyong <guonwpu@qq.com>
2023-05-06 17:42:57 +0800

    use range accessor

--- k2/csrc/pruned_ranges_to_lattice.cu
+++ k2/csrc/pruned_ranges_to_lattice.cu
@@ -58,44 +58,44 @@ FsaVec PrunedRangesToLattice(
     // currently only float type logits is verified.
     TORCH_CHECK(torch::kFloat == logits.scalar_type());
 
-    AT_DISPATCH_FLOATING_TYPES(
-        logits.scalar_type(), "pruned_ranges_to_lattice", ([&] {
+    // AT_DISPATCH_FLOATING_TYPES(
+        // logits.scalar_type(), "pruned_ranges_to_lattice", ([&] {
           auto ranges_a = ranges.accessor<int32_t, 3>();
           auto frames_a = frames.accessor<int32_t, 1>();
           auto symbols_a = symbols.accessor<int32_t, 2>();
-          auto logits_a = logits.accessor<scalar_t, 4>();
-    }));
-    // Typically, U is 5.
-    const int32_t B = ranges.size(0), T = ranges.size(1), U = ranges.size(2);
+          // auto logits_a = logits.accessor<scalar_t, 4>();
+          auto logits_a = logits.accessor<float, 4>();
+    // }));
+
+    // Typically, s_range is 5.
+    const int32_t B = ranges.size(0), T = ranges.size(1), s_range = ranges.size(2);
     const float *logits_data = logits.data_ptr<float>();
     const int32_t *ranges_data = ranges.data_ptr<int32_t>();
     const int32_t *frames_data = frames.data_ptr<int32_t>();
     const int32_t *symbols_data = symbols.data_ptr<int32_t>();
-    const int32_t rng_stride_0 = ranges.stride(0),
-                  rng_stride_1 = ranges.stride(1),
-                  rng_stride_2 = ranges.stride(2);
     const int32_t lg_stride_0 = logits.stride(0),
                   lg_stride_1 = logits.stride(1),
                   lg_stride_2 = logits.stride(2),
                   lg_stride_3 = logits.stride(3);
     K2_CHECK_EQ(frames.numel(), B);
+    // f2s is short for fsa_to_state.
     Array1<int32_t> f2s_row_splits(context, B + 1);
     int32_t * f2s_row_splits_data = f2s_row_splits.Data();
     K2_EVAL(context, B, lambda_set_f2s_row_splits, (int32_t fsa_idx0) {
         int32_t t = frames_data[fsa_idx0];
         K2_CHECK_LE(t, T);
         // + 1 in "t * U + 1" is for super-final state.
-        f2s_row_splits_data[fsa_idx0] = t * U + 1;
+        f2s_row_splits_data[fsa_idx0] = t * s_range + 1;
     });
     ExclusiveSum(f2s_row_splits, &f2s_row_splits);
 
-    RaggedShape fsa_to_states =
+    RaggedShape f2s_shape =
             RaggedShape2(&f2s_row_splits, nullptr, -1);
-    int32_t num_states = fsa_to_states.NumElements();
+    int32_t num_states = f2s_shape.NumElements();
     Array1<int32_t> s2c_row_splits(context, num_states + 1);
     int32_t *s2c_row_splits_data = s2c_row_splits.Data();
-    const int32_t *fts_row_splits1_data = fsa_to_states.RowSplits(1).Data(),
-                  *fts_row_ids1_data = fsa_to_states.RowIds(1).Data();
+    const int32_t *fts_row_splits1_data = f2s_shape.RowSplits(1).Data(),
+                  *fts_row_ids1_data = f2s_shape.RowIds(1).Data();
 
     // set the arcs number for each state
     K2_EVAL(
@@ -103,53 +103,47 @@ FsaVec PrunedRangesToLattice(
           int32_t fsa_idx0 = fts_row_ids1_data[state_idx01],
                   state_idx0x = fts_row_splits1_data[fsa_idx0],
                   state_idx1 = state_idx01 - state_idx0x,
-                  t = state_idx1 / U,
-                  token_index = state_idx1 % U;
+                  t = state_idx1 / s_range,
+                  token_index = state_idx1 % s_range;
 
           K2_CHECK_LE(t, frames_data[fsa_idx0]);
-          if (state_idx1 == frames_data[fsa_idx0] * U - 1) {
-            // frames[fsa_idx0] * U is the state_idx1 of super final-state.
-            // frames[fsa_idx0] * U - 1 is the state pointing to super final-state.
+          if (state_idx1 == frames_data[fsa_idx0] * s_range - 1) {
+            // frames[fsa_idx0] * s_range is the state_idx1 of super final-state.
+            // frames[fsa_idx0] * s_range - 1 is the state pointing to super final-state.
             // final arc to super final state.
             s2c_row_splits_data[state_idx01] = 1;
             return;
           }
-          if (state_idx1 == frames_data[fsa_idx0] * U) {
+          if (state_idx1 == frames_data[fsa_idx0] * s_range) {
             // frames[fsa_idx0] * U is the state_idx1 of super final-state.
             // final state has no leaving arcs.
             s2c_row_splits_data[state_idx01] = 0;
             return;
           }
-          int32_t range_offset = fsa_idx0 * rng_stride_0 + t * rng_stride_1 + token_index * rng_stride_2;
 
-          int32_t next_state_idx1 = -1;
-          // blank connections of last frame is -1
-          // So we need process t == frames_data[fsa_idx0]
+          bool has_horizontal_blank_arc = false;
           if (t < frames_data[fsa_idx0] - 1) {
-            int32_t range_offset_of_lower_bound_of_next_time_step = fsa_idx0 * rng_stride_0 + (t + 1) * rng_stride_1;
-            next_state_idx1 = ranges_data[range_offset] - ranges_data[range_offset_of_lower_bound_of_next_time_step];
+            has_horizontal_blank_arc = ranges_a[fsa_idx0][t][token_index] >= ranges_a[fsa_idx0][t + 1][0];
           }
-          // K2_CHECK_EQ(next_state_idx1_tmp, next_state_idx1);
-          if (token_index < U - 1) {
-            // Typically, U == 5,
+          if (token_index < s_range - 1) {
+            // Typically, s_range == 5,
             // the [0, 1, 2, 3] states for each time step may have a vertial arc plus an optional horizontal blank arc.
             s2c_row_splits_data[state_idx01] = 1;
-            if (next_state_idx1 >= 0) {
+            if (has_horizontal_blank_arc) {
               s2c_row_splits_data[state_idx01] = 2;
             }
           } else {
-            // Typically, U == 5,
-            // the [4] state for each time step have and only have an horizontal blank arc.
-            // K2_CHECK_GE(next_state_idx1, 0);
+            // Typically, s_range == 5,
+            // the [4] state for each time step have and only have a horizontal blank arc.
             s2c_row_splits_data[state_idx01] = 1;
           }
       });
 
     ExclusiveSum(s2c_row_splits, &s2c_row_splits);
-    RaggedShape states_to_arcs =
+    RaggedShape s2a_shape =
             RaggedShape2(&s2c_row_splits, nullptr, -1);
 
-    RaggedShape ofsa_shape = ComposeRaggedShapes(fsa_to_states, states_to_arcs);
+    RaggedShape ofsa_shape = ComposeRaggedShapes(f2s_shape, s2a_shape);
     int32_t num_arcs = ofsa_shape.NumElements();
     Array1<Arc> arcs(context, num_arcs);
     Array1<int32_t> out_map(context, num_arcs);
@@ -170,10 +164,10 @@ FsaVec PrunedRangesToLattice(
                     arc_idx01x = row_splits2_data[state_idx01],
                     state_idx1 = state_idx01 - state_idx0x,
                     arc_idx2 = arc_idx012 - arc_idx01x,
-                    t = state_idx1 / U,
-                    token_index = state_idx1 % U;  // token_index is belong to [0, U)
+                    t = state_idx1 / s_range,
+                    token_index = state_idx1 % s_range;  // token_index is belong to [0, U)
             Arc arc;
-            if (state_idx1 == frames_data[fsa_idx0] * U - 1) {
+            if (state_idx1 == frames_data[fsa_idx0] * s_range - 1) {
               arc.src_state = state_idx1;
               arc.dest_state = state_idx1 + 1;
               arc.label = -1;
@@ -182,13 +176,11 @@ FsaVec PrunedRangesToLattice(
               out_map_data[arc_idx012] = -1;
               return;
             }
-            int32_t range_offset = fsa_idx0 * rng_stride_0 + t * rng_stride_1 + token_index * rng_stride_2;
-            int32_t range_offset_of_lower_bound_of_next_time_step = fsa_idx0 * rng_stride_0 + (t + 1) * rng_stride_1;
             int32_t next_state_idx1, logits_offset;
             arc.src_state = state_idx1;
             // arc.
-            if (token_index < U - 1) {
-              int32_t actual_u = ranges_data[range_offset];
+            if (token_index < s_range - 1) {
+              int32_t actual_u = ranges_a[fsa_idx0][t][token_index];
               int32_t symbols_offset = fsa_idx0 * symbols_stride_0 + actual_u * symbols_stride_1;
               int32_t arc_label =  symbols_data[symbols_offset];
               switch (arc_idx2) {
@@ -200,8 +192,8 @@ FsaVec PrunedRangesToLattice(
                   out_map_data[arc_idx012] = logits_offset;
                   break;
                 case 1:
-                  next_state_idx1 = ranges_data[range_offset] - ranges_data[range_offset_of_lower_bound_of_next_time_step];
-                  arc.dest_state = next_state_idx1 + (t + 1) * U;
+                  next_state_idx1 = ranges_a[fsa_idx0][t][token_index] - ranges_a[fsa_idx0][t + 1][0];
+                  arc.dest_state = next_state_idx1 + (t + 1) * s_range;
                   arc.label = 0;
                   logits_offset = fsa_idx0 * lg_stride_0 + t * lg_stride_1 + token_index * lg_stride_2;
                   arc.score = logits_data[logits_offset];
@@ -212,8 +204,8 @@ FsaVec PrunedRangesToLattice(
               }
             } else {
               K2_CHECK_EQ(arc_idx2, 0);
-              next_state_idx1 = ranges_data[range_offset] - ranges_data[range_offset_of_lower_bound_of_next_time_step];
-              arc.dest_state = next_state_idx1 + (t + 1) * U;
+              next_state_idx1 = ranges_a[fsa_idx0][t][token_index] - ranges_a[fsa_idx0][t + 1][0];
+              arc.dest_state = next_state_idx1 + (t + 1) * s_range;
               arc.label = 0;
               logits_offset = fsa_idx0 * lg_stride_0 + t * lg_stride_1 + token_index * lg_stride_2;
               arc.score = logits_data[logits_offset];
