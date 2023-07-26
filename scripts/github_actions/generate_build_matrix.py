ac9c9b69d07594d7e2a1cf50bb1e29350175ac4b
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-26 18:28:08 +0800

    fix style issues

--- scripts/github_actions/generate_build_matrix.py
+++ scripts/github_actions/generate_build_matrix.py
@@ -199,7 +199,7 @@ def generate_build_matrix(enable_cuda, for_windows, for_macos, test_only_latest_
                         {
                             "torch": torch,
                             "python-version": p,
-                            "image": f"pytorch/manylinux-builder:cuda10.2",
+                            "image": "pytorch/manylinux-builder:cuda10.2",
                         }
                     )
 
