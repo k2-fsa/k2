cb429acbbc8164633934f13160712db767055053
pkufool <wkang.pku@gmail.com>
2023-09-26 20:03:46 +0800

    fix install torch

--- scripts/github_actions/install_torch.sh
+++ scripts/github_actions/install_torch.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ $TORCH_VERSION != "" ] && [ $CUDA_VERSION != ""]; then
+if [ $TORCH_VERSION != "" ] && [ $CUDA_VERSION != "" ]; then
     torch=$TORCH_VERSION
     cuda=$CUDA_VERSION
 fi
