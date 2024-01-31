5f547eca10cc7edf4306b9c8dc1638bffcaae3fc
Fangjun Kuang <csukuangfj@gmail.com>
2024-01-31 09:20:49 +0800

    Build wheels for torch 2.2.0

--- scripts/github_actions/generate_build_matrix.py
+++ scripts/github_actions/generate_build_matrix.py
@@ -178,10 +178,16 @@ def generate_build_matrix(
             if not for_windows
             else ["11.8.0", "12.1.0"],
         },
+        "2.2.0": {
+            "python-version": ["3.8", "3.9", "3.10", "3.11", "3.12"],
+            "cuda": ["11.8", "12.1"]  # default 12.1
+            if not for_windows
+            else ["11.8.0", "12.1.0"],
+        },
         # https://github.com/Jimver/cuda-toolkit/blob/master/src/links/windows-links.ts
     }
     if test_only_latest_torch:
-        latest = "2.1.2"
+        latest = "2.2.0"
         matrix = {latest: matrix[latest]}
 
     if for_windows or for_macos:
@@ -210,6 +216,8 @@ def generate_build_matrix(
         matrix["1.13.1"] = {"python-version": ["3.7", "3.8", "3.9", "3.10"]}
         matrix["2.0.0"] = {"python-version": ["3.8", "3.9", "3.10", "3.11"]}
         matrix["2.0.1"] = {"python-version": ["3.8", "3.9", "3.10", "3.11"]}
+        # TODO(fangjun): we currently don't support macOS M1 build
+        # since github actions does not support it.
 
     ans = []
     for torch, python_cuda in matrix.items():
