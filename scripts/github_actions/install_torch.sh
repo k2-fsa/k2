9a1ce76af8a8c7b967d15cacb26fd20792a9e4f6
Fangjun Kuang <csukuangfj@gmail.com>
2024-04-25 09:50:01 +0800

    small fixes

--- scripts/github_actions/install_torch.sh
+++ scripts/github_actions/install_torch.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
+
 if [ x"$TORCH_VERSION" != x"" ] && [ x"$CUDA_VERSION" != x"" ]; then
     torch=$TORCH_VERSION
     cuda=$CUDA_VERSION
